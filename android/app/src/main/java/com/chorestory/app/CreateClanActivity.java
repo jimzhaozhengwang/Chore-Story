2f89dee985854638ca3ab098ddfba837d6712b0b
Jim Wang <zhawang@zynga.com>
2019-06-22 16:13:30 -0400

    Initialized created clan activity

---
+++ android/app/src/main/java/com/chorestory/app/CreateClanActivity.java
@@ -0,0 +1,53 @@
+package com.chorestory.app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.chorestory.R;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class CreateClanActivity extends ChoreStoryActivity {
+
+    private String clanName;
+    private String username;
+    private String password;
+
+    private EditText clanNameEditText;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button signUpButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        App.getAppComponent().inject(this);
+        setContentView(R.layout.activity_create_clan);
+
+        clanNameEditText = findViewById(R.id.clan_name_edit_text);
+        usernameEditText = findViewById(R.id.username_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+
+        signUpButton = findViewById(R.id.sign_up_button);
+        buttons = Collections.singletonList(signUpButton);
+
+        enableButtons();
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideKeyBoard();
+                disableButtons();
+                clanName = clanNameEditText.getText().toString();
+                username = usernameEditText.getText().toString();
+                password = passwordEditText.getText().toString();
+
+                // TODO: verification
+                //  if successful register new clan & user, else display snackbar/toast
+            }
+        });
+    }
+}
