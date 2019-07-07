98fa12c5bd4a97ae38ada489bec5fd3e5293d513
isabelleleeson <isabelle.leeson@hotmail.com>
2019-07-06 21:58:35 -0400

    populate parent upcoming quests

---
+++ android/app/src/main/java/com/chorestory/model/QuestRecyclerViewItem.java
@@ -0,0 +1,56 @@
+package com.chorestory.model;
+
+import com.chorestory.R;
+import com.chorestory.module.QuestCompletion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestRecyclerViewItem {
+
+    private String name;
+    private int imageId;
+    private int exp;
+    private String owner;
+    private String description;
+    private boolean mandatory;
+    private int dueDate;
+
+    public static List<QuestRecyclerViewItem> getData(QuestCompletion questCompletion) {
+        List<QuestRecyclerViewItem> dataList = new ArrayList<>();
+        // TODO: fetch quests using QuestCompletion
+        for (int i = 0; i < 2; ++i) {
+            QuestRecyclerViewItem item = new QuestRecyclerViewItem();
+            item.name = "Sweep the floor";
+            item.imageId = R.drawable.broom;
+            item.exp = 30;
+            item.owner = "Isabelle";
+            item.description = "sweep it for 10 hours plz";
+            item.mandatory = true;
+            item.dueDate = 5;
+            dataList.add(item);
+        }
+        return dataList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public String getOwner() { return owner; }
+
+    public String getDescription() { return description; }
+
+    public boolean getMandatory() { return mandatory; }
+
+    public int getDueDate() { return dueDate; }
+
+}
