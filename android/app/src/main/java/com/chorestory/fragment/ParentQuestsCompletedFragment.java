19b2289d2d9837037ce5bbd8c8fd9c1816676304
isabelleleeson <isabelle.leeson@hotmail.com>
2019-07-09 17:38:16 -0400

    populate pending and completed quests

--- android/app/src/main/java/com/chorestory/fragment/ParentQuestsCompletedFragment.java
+++ android/app/src/main/java/com/chorestory/fragment/ParentQuestsCompletedFragment.java
@@ -2,17 +2,34 @@ package com.chorestory.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.chorestory.R;
+import com.chorestory.adapter.QuestRecyclerViewAdapter;
+import com.chorestory.model.QuestRecyclerViewItem;
+import com.chorestory.module.QuestCompletion;
 
 public class ParentQuestsCompletedFragment extends Fragment {
+
+    private RecyclerView completedQuestsRecyclerView;
+    private RecyclerView.Adapter completedQuestsAdapter;
+    private RecyclerView.LayoutManager completedQuestsLayoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_parent_quests_completed, container, false);
+
+        // Completed Quests
+        completedQuestsRecyclerView = view.findViewById(R.id.completed_quests_recycler_view);
+        completedQuestsLayoutManager = new LinearLayoutManager(getActivity());
+        completedQuestsRecyclerView.setLayoutManager(completedQuestsLayoutManager);
+
+        completedQuestsAdapter = new QuestRecyclerViewAdapter(QuestRecyclerViewItem.getData(QuestCompletion.COMPLETED));
+        completedQuestsRecyclerView.setAdapter(completedQuestsAdapter);
 
         return view;
     }
--- android/app/src/main/java/com/chorestory/fragment/ParentQuestsPendingFragment.java
+++ android/app/src/main/java/com/chorestory/fragment/ParentQuestsPendingFragment.java
@@ -2,17 +2,34 @@ package com.chorestory.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.chorestory.R;
+import com.chorestory.adapter.QuestRecyclerViewAdapter;
+import com.chorestory.model.QuestRecyclerViewItem;
+import com.chorestory.module.QuestCompletion;
 
 public class ParentQuestsPendingFragment extends Fragment {
+
+    private RecyclerView pendingQuestsRecyclerView;
+    private RecyclerView.Adapter pendingQuestsAdapter;
+    private RecyclerView.LayoutManager pendingQuestsLayoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_parent_quests_pending, container, false);
+
+        // Pending Quests
+        pendingQuestsRecyclerView = view.findViewById(R.id.pending_quests_recycler_view);
+        pendingQuestsLayoutManager = new LinearLayoutManager(getActivity());
+        pendingQuestsRecyclerView.setLayoutManager(pendingQuestsLayoutManager);
+
+        pendingQuestsAdapter = new QuestRecyclerViewAdapter(QuestRecyclerViewItem.getData(QuestCompletion.PENDING));
+        pendingQuestsRecyclerView.setAdapter(pendingQuestsAdapter);
 
         return view;
     }
