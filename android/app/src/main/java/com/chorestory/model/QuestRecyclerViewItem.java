2b0d4e0428c1e722f06859c9202f9a7716ef050d
Isabelle Leeson <isabelle.leeson@hotmail.com>
2019-07-11 14:04:08 -0400

    populate parent upcoming, pending and completed quests (#32)
    * populate parent upcoming quests
    
    * populate pending and completed quests
    
    * clean up xml
    
    * fix quest constructor
    
    * add strings to strings.xml
    
    * reformat

---
+++ android/app/src/main/java/com/chorestory/model/QuestRecyclerViewItem.java
@@ -0,0 +1,67 @@
+package com.chorestory.model;
+
+import com.chorestory.R;
+import com.chorestory.module.QuestCompletion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestRecyclerViewItem {
+
+    private String name;
+    private int imageId;
+    private int exp;
+    private String owner;
+    private String description;
+    private boolean mandatory;
+    private int dueDate;
+
+    public static List<QuestRecyclerViewItem> getData(QuestCompletion questCompletion) {
+        List<QuestRecyclerViewItem> dataList = new ArrayList<>();
+        // TODO: fetch quests using QuestCompletion
+        for (int i = 0; i < 2; ++i) {
+            QuestRecyclerViewItem item = new QuestRecyclerViewItem("Sweep the floor", R.drawable.broom, 30, "Isabelle", "sweep it for 10 hours plz", true, 5);
+            dataList.add(item);
+        }
+        return dataList;
+    }
+
+    QuestRecyclerViewItem(String name, int imageId, int exp, String owner, String description, boolean mandatory, int dueDate) {
+        this.name = name;
+        this.imageId = imageId;
+        this.exp = exp;
+        this.owner = owner;
+        this.description = description;
+        this.mandatory = mandatory;
+        this.dueDate = dueDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean getMandatory() {
+        return mandatory;
+    }
+
+    public int getDueDate() {
+        return dueDate;
+    }
+
+}
