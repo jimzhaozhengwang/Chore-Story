52d723936e007fd6a8ba8f2634b5c6fbd631cd08
isabelleleeson <isabelle.leeson@hotmail.com>
2019-07-19 21:19:30 -0400

    reformat code

--- android/app/src/main/java/com/chorestory/adapter/QuestRecyclerViewAdapter.java
+++ android/app/src/main/java/com/chorestory/adapter/QuestRecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.chorestory.adapter;
 
 import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
