2eb3b25c9b04f73cc05f53ed0fe20261ceb08132
Mark Keller <markooo.keller@gmail.com>
2019-07-21 19:34:26 -0400

    dialogflow fix

--- server/backbone/dialogflow.py
+++ server/backbone/dialogflow.py
@@ -5,17 +5,19 @@ from google.protobuf.json_format import MessageToDict
 
 
 def call_dialogflow(text):
-    project_id = current_app.config['DIALOGFLOW_PROJECT_ID']
-    session_id = current_user.name
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
+    project_id = current_app.config.get('DIALOGFLOW_PROJECT_ID')
+    if project_id:
+        session_id = current_user.name
+        session_client = dialogflow.SessionsClient()
+        session = session_client.session_path(project_id, session_id)
 
-    text_input = dialogflow.types.TextInput(
-        text=text, language_code='en')
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code='en')
 
-    query_input = dialogflow.types.QueryInput(text=text_input)
+        query_input = dialogflow.types.QueryInput(text=text_input)
 
-    response = session_client.detect_intent(
-        session=session, query_input=query_input)
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
 
-    return MessageToDict(response.query_result.parameters)
+        return MessageToDict(response.query_result.parameters)
+    return None
