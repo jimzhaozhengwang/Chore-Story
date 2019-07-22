e0ec57eb2029e2faecf3e90393c5c362c658bb89
Cristian David <cr2david@edu.uwaterloo.ca>
2019-07-21 21:31:12 -0400

    uncomment test lines

--- android/app/src/main/java/com/chorestory/app/ParentSignUpActivity.java
+++ android/app/src/main/java/com/chorestory/app/ParentSignUpActivity.java
@@ -60,28 +60,28 @@ public class ParentSignUpActivity extends ChoreStoryActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        if (requestCode == REQUEST_CODE &&
-//                resultCode == CommonStatusCodes.SUCCESS &&
-//                data != null) {
-//
-//            Barcode barCode = data.getParcelableExtra(getString(R.string.qr_code));
-//
-//            if (barCode != null && !barCode.displayValue.isEmpty()) {
-//
-//                if (barCode.displayValue.endsWith(getString(R.string.parent_register_identifier))) {
+        if (requestCode == REQUEST_CODE &&
+                resultCode == CommonStatusCodes.SUCCESS &&
+                data != null) {
+
+            Barcode barCode = data.getParcelableExtra(getString(R.string.qr_code));
+
+            if (barCode != null && !barCode.displayValue.isEmpty()) {
+
+                if (barCode.displayValue.endsWith(getString(R.string.parent_register_identifier))) {
 
                     // The token can be used by the child to login
                     tokenHandler.setParentRegistrationToken(
-                            "31f056a8-d760-491d-abd1-31b3e87dd90e:parentRegister".replace(getString(R.string.parent_register_identifier), ""));
+                            barCode.displayValue.replace(getString(R.string.parent_register_identifier), ""));
                     navigateTo(ParentJoinClanActivity.class);
 
-//                }
-//
-//            } else {
-//                Toaster.showToast(this, this.getString(R.string.unable_to_detect_qr_code));
-//            }
-//        } else {
-//            Toaster.showToast(this, this.getString(R.string.unable_to_detect_qr_code));
-//        }
+                }
+
+            } else {
+                Toaster.showToast(this, this.getString(R.string.unable_to_detect_qr_code));
+            }
+        } else {
+            Toaster.showToast(this, this.getString(R.string.unable_to_detect_qr_code));
+        }
     }
 }
